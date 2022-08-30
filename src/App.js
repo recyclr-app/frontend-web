047806e7cafcc0b05511f78a56bafea2aee7f6a3
Stephen Wong <wongstephenky@gmail.com>
2022-08-30 09:05:18 -0700

    updated site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from "./components/Home";
+import HomeAlt from "./components/HomeAlt";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <HomeAlt />
     </div>
   );
 }
---
+++ src/components/HomeAlt.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function HomeAlt() {
+  return (
+    <div className="alt__container">
+      <h1 className="home__h1">
+        recycl<span className="home__h1--accent">r</span>
+      </h1>
+      <h2 className="home__h2">
+        Reducing, reusing and recycling are the easiest ways to save energy,
+        money and natural resources - creating a healthier environment for all
+        of us.
+      </h2>
+
+      <div className="alt__hero-container" s>
+        <img
+          alt="Find it on the app store"
+          src={require("../assets/shots.png")}
+          className="alt__shots"
+        />
+      </div>
+      <div className="alt__demo-container">
+        <a href="https://appetize.io/app/4d65avcuv7tvcl4ruibauomqm4?device=iphone12promax&osVersion=14.5&scale=75">
+          <button className="alt__demo-button">
+            <span>Demo</span>
+          </button>
+        </a>
+        <br />
+      </div>
+      <div className="alt__hero-container" s>
+        <img
+          alt="Find it on the app store"
+          src={require("../assets/hero.png")}
+          className="alt__hero"
+        />
+      </div>
+      <footer>
+        <img
+          alt="Find it on the app store"
+          src={require("../assets/whiteapplogo.png")}
+          className="alt__appStore-logo"
+        />
+        <br />© Copyright recyclr {new Date().getFullYear()}. All Rights
+        Reserved
+      </footer>
+    </div>
+  );
+}
